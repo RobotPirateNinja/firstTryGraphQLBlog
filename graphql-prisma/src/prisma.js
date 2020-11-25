feb934c8c16c4116f2cd7d856a9ad4e7aeffeeed
RobotPirateNinja <roy.m.taylor@gmail.com>
2020-11-25 11:10:05 -0700

    Test for other laptop

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -8,4 +8,18 @@ const prisma = new Prisma({
 prisma.query.comments(null, ' {id author {name id} text post {id title}}').then((data) => {
     console.log(JSON.stringify(data,undefined,2))
 } )
-
+ 
+prisma.mutation.createPost({
+    data: {
+        title: "New Post via Node",
+        body: "You can find the new post here",
+        published: true,
+        author: {
+            connect: {
+                id: "ckhllewwj005r0707aady7f4r"
+            }
+        }
+    }
+}, '{ id, title, body, published }').then((data) => {
+    console.log(data)
+})
